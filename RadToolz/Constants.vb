723c67deb01f85b99f14f2ce7137fd6f526251c7
Backscatter enterprises <support@radtoolz.com>
2024-12-29 08:14:13 -0500

    Minor editorial updates to comments.

--- RadToolz/Constants.vb
+++ RadToolz/Constants.vb
@@ -4,7 +4,7 @@ Module Constants
     '* Author:      Backscatter enterprises
     '* Date:        12/24/2024
 
-    Public Const RadToolzVersion As Double = 3.51
+    Public Const RadToolzVersion As Double = 3.52
     Public Const RadToolzPreRelease As String = "" 'α ß Γ π Σ σ µ
     Public Const maxBranches As Integer = 50
     Public iExcel As Object = ExcelDnaUtil.Application
--- RadToolz/My Project/AssemblyInfo.vb
+++ RadToolz/My Project/AssemblyInfo.vb
@@ -32,5 +32,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")>
 
 <Assembly: AssemblyVersion("0.0.0.0")>
-<Assembly: AssemblyFileVersion("3.11.1.0")>
+<Assembly: AssemblyFileVersion("3.5.1.0")>
 <Assembly: NeutralResourcesLanguageAttribute("en-US")>
